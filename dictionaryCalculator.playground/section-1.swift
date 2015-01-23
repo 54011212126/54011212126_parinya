3da57b87ed172ed7e12ef87e111358c6cfe521f2
Siradafilm_55011212027 <istudents@iClassrooms-iMac-30.local>
2015-01-23 16:25:39 +0700

    Week2
    23-01-2015

--- dictionaryCalculator.playground/section-1.swift
+++ dictionaryCalculator.playground/section-1.swift
@@ -24,24 +24,23 @@ class TipCalculator {
         return subtotal * tipPct
     }
     
+    
+    
     //1
     func returnPossibleTips()->[Int: Double]{
         let possibleTipsInferred = [0.15, 0.18, 0.20]
         let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
-        var numberofItems = possibleTipsInferred.count
         
-    //2
-    var retval = [Int: Double]()
-    for possibleTip in possibleTipsInferred{
-        let intPct = Int(possibleTip*100)
-        //3
-        retval[intPct] = calcTipWithTipPct(possibleTip)
+        //2
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred{
+            let intPct = Int(possibleTip*100)
+            //3
+            retval[intPct] = calcTipWithTipPct(possibleTip)
         }
         return retval
     }
-
 }
 //
 let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
 tipCalc.returnPossibleTips()
-
