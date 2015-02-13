35636e1f88343b8a7be0025761d70cc611907aaa
54011212126 <istudents@iClassrooms-iMac-47.local>
2015-02-13 17:18:24 +0700

    WEEK5
    13022015

---
+++ Tuple.playground/section-1.swift
@@ -0,0 +1,10 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let tipAndTotalNamed = (tipAmt:4.00, total:22.19)
+tipAndTotalNamed.tipAmt
+tipAndTotalNamed.total
+
