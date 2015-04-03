e39ef57e480ebc285ad989bde0c3186b6ae2ef00
54011212126 <istudents@iClassrooms-iMac-47.local>
2015-04-03 16:26:00 +0700

    Week11Last
    03042015

---
+++ UIImagePickerController/UIImagePickerController/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  UIImagePickerController
+//
+//  Created by iStudents on 4/3/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    var imagePicker: UIImagePickerController!
+    var newMedia: Bool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet var imageView: UIImageView!
+
+    @IBAction func takePhoto(sender: AnyObject) {
+        
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .Camera
+        imagePicker.mediaTypes = [kUTTypeImage as NSString]
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+        newMedia = true
+    }
+
+    @IBAction func library(sender: AnyObject) {
+    imagePicker.allowsEditing = false
+        imagePicker.sourceType = .Camera
+        imagePicker.mediaTypes = [kUTTypeImage as NSString]
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+        newMedia = true
+        
+    }
+    
+   func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        dismissViewControllerAnimated(true, completion: nil)
+    
+        let mediaType = info[UIImagePickerControllerMediaType] as NSString
+        if(mediaType.isEqualToString(kUTTypeImage as NSString)){
+            let image = info[UIImagePickerControllerOriginalImage] as UIImage
+            imageView.image == true
+            
+            if (newMedia == true){
+                UIImageWriteToSavedPhotosAlbum(image, self, "image: didFinishSavingWithError: contextInfo:", nil)
+            }
+        }
+    
+    }
+    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo: UnsafeBufferPointer<Void>){
+    
+        if(error != nil){
+            let alert = UIAlertController(title: "Save Failed", message: "Failed to save image", preferredStyle: UIAlertControllerStyle.Alert)
+            let cancelAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+    }
+}
+
--- labfishAnimation1/Fish/ViewController.swift
+++ labfishAnimation1/Fish/ViewController.swift
@@ -1,27 +1,18 @@
 //
 //  ViewController.swift
-//  labfishAnimation
+//  Fish
 //
 //  Created by iStudents on 3/27/15.
-//  Copyright (c) 2015 iStudents. All rights reserved.
+//  Copyright (c) 2015 Jay. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UICollisionBehaviorDelegate{
-
-   // var coloredSquare: UIView!
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
     
-//    let options = UIViewAnimationOptions.Autoreverse | UIViewAnimationOptions.Repeat | UIViewAnimationOptions.CurveEaseInOut
-    
-//    var firstContact = false
-//    var collision: UICollisionBehavior!
-//    var gravity : UIGravityBehavior!
-//    var animator: UIDynamicAnimator!
-  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         let square = UIView()
         square.backgroundColor = UIColor.blueColor()
         square.frame = CGRect(x: 0, y: 175, width: 70, height: 70)
@@ -33,36 +24,43 @@ class ViewController: UIViewController, UICollisionBehaviorDelegate{
                 square.removeFromSuperview()
                 
         })
-    }
+       
 
-    @IBOutlet weak var numberOfFishSlider: UISlider!
-    @IBAction func animateButtonPressed(sender: AnyObject) {
         
+    }
+    
+    @IBOutlet weak var numberOfFishSlider: UISlider!
+    
+    @IBAction func animateButtonPressed(sender: AnyObject) {
         let numberOfFish = Int(self.numberOfFishSlider.value)
         for loopNumber in 1...numberOfFish{
-            
             let duration = 3.0
             let options = UIViewAnimationOptions.CurveLinear
-            let delay = NSTimeInterval(10 + arc4random_uniform(100)) / 1000
+            
+            let delay = NSTimeInterval(100 + arc4random_uniform(100)) / 1000
             let size : CGFloat = CGFloat( arc4random_uniform(40))+20
             let yPosition : CGFloat = CGFloat( arc4random_uniform(200))+20
-        
+            
             let fish = UIImageView()
             fish.image = UIImage(named: "fish.jpg")
             fish.frame = CGRectMake(0-size, yPosition, size, size)
             self.view.addSubview(fish)
-        
+            
             UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
-                fish.frame = CGRectMake(320, yPosition, size, size)
-                },completion: { animationFinished in
+               
+                fish.frame = CGRectMake(320-size, yPosition, size, size)
+                
+                }, completion: { animationFinished in
                     fish.removeFromSuperview()
+                    
             })
         }
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 }
-
-
